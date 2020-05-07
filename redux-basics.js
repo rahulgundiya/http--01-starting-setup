17dfa239f676d4699f902e4af8b864a2a4677a5f
Rahul <rahulgundiya34@gmail.com>
2020-05-07 14:07:04 +0530

    Addin Subscription

--- redux-basics.js
+++ redux-basics.js
@@ -24,6 +24,11 @@ const rootReducer = (state=initialState , action)=>{
 const store = createStore(rootReducer);
 console.log(store.getState());
 
+//Subscription
+store.subscribe(()=>{
+    console.log('[Subscription]' , store.getState())
+})
+
 //Dispatch
 store.dispatch({type:'INC_COUNTER'})
 store.dispatch({type:'ADD_COUNTER' , value:10});
