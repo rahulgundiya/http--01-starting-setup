c9f8d965fb683f02abca462c579f2e657ca64635
Rahul <rahulgundiya34@gmail.com>
2020-05-25 21:20:06 +0530

    Using action creator and get state

--- src/store/actions/result.js
+++ src/store/actions/result.js
@@ -8,8 +8,10 @@ export const saveResult=(res)=>{
     }
 }
 export const storeResult=(res)=>{
-    return dispatch=>{
+    return (dispatch,getState)=>{
         setTimeout(()=>{
+            const oldCounter=getState().ctr.counter;
+            console.log('Hii oldCounter' ,oldCounter)
             dispatch(saveResult(res))
 
         } ,2000)
